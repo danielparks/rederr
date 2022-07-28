75e117c64081ca4ca77cfb0ea2ab96e1eab5c0b6
Daniel Parks <dp+git@oxidized.org>
2022-07-27 17:44:06 -0700

    Test switching between stderr and stdout on the same line.

--- stdouterr.sh
+++ stdouterr.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/bin/zsh
 # It's often useful to have something that outputs to both stdout and stderr
 
 echo 1 stdout
 echo 2 STDERR >&2
 echo 3 stdout
 
-echo 4 STDERR: sleep 1.. >&2
-sleep 1
+echo 4 STDERR: sleep 0.1 >&2
+sleep 0.1
 
-echo 5 stdout: sleep 1..
-sleep 1
+echo 5 stdout: sleep 0.1
+sleep 0.1
 
-echo 6 STDERR >&2
-echo 7 stdout
+echo -n "6 STDERR " >&2
+echo -n "7 stdout "
 echo 8 STDERR >&2
 
 kill $$
