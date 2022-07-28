568aa4f4764e06a30fbe02c8e40e80f666d318aa
Daniel Parks <dp+git@oxidized.org>
2022-07-27 22:30:57 -0700

    Clean up; remove simplelog.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,20 @@
 use clap::Parser;
 use popol;
-use simplelog::{
-    ColorChoice, CombinedLogger, Config, ConfigBuilder, LevelFilter,
-    TermLogger, TerminalMode,
-};
+use std::ffi::OsString;
 use std::io::{Read, Write};
-use std::path::PathBuf;
-use std::process::{Command, Stdio};
+use std::process;
 use std::time::Duration;
-use termcolor::{Color, ColorSpec, WriteColor};
+use termcolor::{Color, ColorChoice, ColorSpec, WriteColor};
 
-// FIXME UNIX only
+// FIXME UNIX only (is popol UNIX only?)
 use std::os::unix::process::ExitStatusExt;
 
 #[derive(Debug, clap::Parser)]
 #[clap(version, about)]
 struct Params {
     /// Executable to run
-    #[clap(parse(from_os_str))]
-    command: PathBuf,
-    /// Verbosity (may be repeated up to three times)
-    #[clap(short, long, parse(from_occurrences))]
-    verbose: u8,
+    #[clap()]
+    command: OsString,
     /// Timeout on individual reads (e.g. "1s", "1h", or "30ms")
     #[clap(long, name="duration", parse(try_from_str = duration_str::parse))]
     idle_timeout: Option<Duration>,
@@ -30,34 +23,19 @@ struct Params {
 fn main() {
     if let Err(error) = cli(Params::parse()) {
         eprintln!("Error: {:#}", error);
-        std::process::exit(1);
+        process::exit(1);
     }
 }
 
 fn cli(params: Params) -> anyhow::Result<()> {
-    let filter = match params.verbose {
-        3.. => LevelFilter::Trace,
-        2 => LevelFilter::Debug,
-        1 => LevelFilter::Info,
-        0 => LevelFilter::Warn,
-    };
-
-    CombinedLogger::init(vec![
-        // Default logger
-        new_term_logger(filter, new_logger_config().build()),
-    ])
-    .unwrap();
-
-    println!("command: {:?}", params.command);
-
-    let mut child = Command::new(params.command)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
+    let mut child = process::Command::new(params.command)
+        .stdout(process::Stdio::piped())
+        .stderr(process::Stdio::piped())
         .spawn()?;
 
     let mut buffer = [0; 1024]; // FIXME: best buffer size?
     let mut sources = popol::Sources::with_capacity(2);
-    let mut events = popol::Events::with_capacity(2);
+    let mut events = popol::Events::new();
 
     let mut child_out = child.stdout.take().expect("child.stdout is None");
     sources.register(1, &child_out, popol::interest::READ);
@@ -66,10 +44,8 @@ fn cli(params: Params) -> anyhow::Result<()> {
     sources.register(2, &child_err, popol::interest::READ);
 
     // FIXME? check if it’s a TTY?
-    let mut stdout =
-        termcolor::StandardStream::stdout(termcolor::ColorChoice::Auto);
-    let mut stderr =
-        termcolor::StandardStream::stderr(termcolor::ColorChoice::Auto);
+    let mut stdout = termcolor::StandardStream::stdout(ColorChoice::Auto);
+    let mut stderr = termcolor::StandardStream::stderr(ColorChoice::Auto);
     let mut err_color = ColorSpec::new();
     err_color.set_fg(Some(Color::Red));
     err_color.set_intense(true);
@@ -113,8 +89,8 @@ fn cli(params: Params) -> anyhow::Result<()> {
     }
 
     let status = child.wait().expect("failed to wait on child");
-    std::process::exit(
-        code_for_wait_status(status).expect("no exit code or signal for child"),
+    process::exit(
+        wait_status_to_code(status).expect("no exit code or signal for child"),
     );
 }
 
@@ -135,17 +111,7 @@ where
     // FIXME? handle if err.kind() == io::ErrorKind::TimedOut
 }
 
-fn code_for_wait_status(status: std::process::ExitStatus) -> Option<i32> {
+/// Get the actual exit code from a finished child process
+fn wait_status_to_code(status: process::ExitStatus) -> Option<i32> {
     status.code().or_else(|| Some(128 + status.signal()?))
-}
-
-fn new_term_logger(level: LevelFilter, config: Config) -> Box<TermLogger> {
-    TermLogger::new(level, config, TerminalMode::Mixed, ColorChoice::Auto)
-}
-
-fn new_logger_config() -> ConfigBuilder {
-    let mut builder = ConfigBuilder::new();
-    builder.set_time_to_local(true);
-    builder.set_target_level(LevelFilter::Error);
-    builder
 }
