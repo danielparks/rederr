5ef9bf41d2e065d29ccdf8d222b0acc16d946cb4
Daniel Parks <dp+git@oxidized.org>
2022-07-28 16:47:25 -0700

    Make stdouterr.sh output multiples of 10 bytes.

--- stdouterr.sh
+++ stdouterr.sh
@@ -1,21 +1,21 @@
 #!/bin/zsh
 # It's often useful to have something that outputs to both stdout and stderr
 
-echo 1 stdout
-echo 2 stdout
-echo 3 STDERR >&2
+echo 01 stdout
+echo 02 stdout
+echo 03 STDERR >&2
 
-echo 4 STDERR: sleep 0.1 >&2
+echo 04 STDERR: sleep 0.1 >&2
 sleep 0.1
 
-echo 5 stdout: sleep 0.1
+echo 05 stdout: sleep 0.1
 sleep 0.1
 
-echo -n "6 STDERR " >&2
-echo -n "7 stdout "
-echo 8 STDERR >&2
+echo -n "06 STDERR " >&2
+echo -n "07 stdout "
+echo 08 STDERR >&2
 
-echo -n "9 STDERR sleep 0.1 " >&2
+echo -n "09 STDERR sleep 0.1 " >&2
 sleep 0.1
 echo -n "10 stdout sleep 0.1 "
 sleep 0.1
