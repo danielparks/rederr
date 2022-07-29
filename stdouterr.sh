a01568f54487cbc39dd1bb369afade6acdd16328
Daniel Parks <dp+git@oxidized.org>
2022-07-28 17:47:25 -0700

    Tweak stdouterr.sh to actually make it output multiples of 10 bytes.

--- stdouterr.sh
+++ stdouterr.sh
@@ -5,10 +5,10 @@ echo 01 stdout
 echo 02 stdout
 echo 03 STDERR >&2
 
-echo 04 STDERR: sleep 0.1 >&2
+echo 04 STDERR sleep 0.1 >&2
 sleep 0.1
 
-echo 05 stdout: sleep 0.1
+echo 05 stdout sleep 0.1
 sleep 0.1
 
 echo -n "06 STDERR " >&2
