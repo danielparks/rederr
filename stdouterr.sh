2035c0a7f1f1bf0116e24b0fcef10b550d3db9bc
Daniel Parks <dp+git@oxidized.org>
2022-12-15 17:39:12 -0800

    stdouterr.sh: make it easier to test timeouts in the middle of a line.

--- stdouterr.sh
+++ stdouterr.sh
@@ -15,8 +15,8 @@ echo -n "06 STDERR " >&2
 echo -n "07 stdout "
 echo 08 STDERR >&2
 
-echo -n "09 STDERR sleep 0.1 " >&2
-sleep 0.1
+echo -n "09 STDERR sleep 0.2 " >&2
+sleep 0.2
 echo -n "10 stdout sleep 0.1 "
 sleep 0.1
 echo 11 STDERR >&2
